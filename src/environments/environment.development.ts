cc42833b02b563bfd7c573427bf67b2bc3ba5f24
tkderphu <quangphu.re050204@gmail.com>
2024-10-26 11:35:45 +0700

    r

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
-const BASE_URL = "http://103.109.37.30:8080"
+const BASE_URL = "http://localhost:8080"
 //"http://103.109.37.30:8080"
 export const environment = {
   REST_API:  `${BASE_URL}/api/v1`,
