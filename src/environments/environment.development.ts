0f4e59458a865ab66785c4a8c0e9bb69a0927a12
tkderphu <quangphu.re050204@gmail.com>
2024-10-29 21:41:00 +0700

    edit: domain

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
-const BASE_URL = "http://app-api:8080"
+const domain = 'viosmash.site'
 //"http://103.109.37.30:8080"
 export const environment = {
-  REST_API:  `${BASE_URL}/api/v1`,
-  WEB_SOCKET: `${BASE_URL}/ws`
+  REST_API:  `http://${domain}:8080/api/v1`,
+  WEB_SOCKET: `http://${domain}:8080/ws`
 };
 
