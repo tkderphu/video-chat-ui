a29031f002e4475bffd0b96e24ca965cfffe48f1
tkderphu <quangphu.re050204@gmail.com>
2024-10-30 10:12:51 +0700

    Revert "edit: domain"
    This reverts commit 0f4e59458a865ab66785c4a8c0e9bb69a0927a12.

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
-const domain = 'viosmash.site'
+const BASE_URL = "http://app-api:8080"
 //"http://103.109.37.30:8080"
 export const environment = {
-  REST_API:  `http://${domain}:8080/api/v1`,
-  WEB_SOCKET: `http://${domain}:8080/ws`
+  REST_API:  `${BASE_URL}/api/v1`,
+  WEB_SOCKET: `${BASE_URL}/ws`
 };
 
