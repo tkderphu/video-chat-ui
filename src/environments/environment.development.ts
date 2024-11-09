3fd8b5014de5289a66a92f1d0c7cd33a9d509712
tkderphu <quangphu.re050204@gmail.com>
2024-11-09 23:44:02 +0700

    ok

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,6 +3,6 @@ const BASE_URL = 'https://viosmash.site'
 //"http://103.109.37.30:8080"
 export const environment = {
   REST_API:  `${BASE_URL}/api/v1`,
-  WEB_SOCKET: `${BASE_URL}/ws`
+  WEB_SOCKET: `${BASE_URL}/`
 };
 
