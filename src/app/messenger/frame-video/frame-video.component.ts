883e09e4c4273bb3281db424ae6dad6819ce1366
tkderphu <quangphu.re050204@gmail.com>
2024-11-18 20:00:43 +0700

    explain code

--- src/app/messenger/frame-video/frame-video.component.ts
+++ src/app/messenger/frame-video/frame-video.component.ts
@@ -125,67 +125,86 @@ export class FrameVideoComponent implements OnInit {
         this.socket.on(
           "/topic/room",
           (payload: any) => {
-            console.log(payload)
+            /**
+             * Tin hieu duoc gui tu server
+             */
             const signalPayload: SignalPayload = JSON.parse(payload)
+
+            /**
+             * signalPayload.dest: phong room | user
+             * Giai thich:
+             * phong room: muon noi la co user da vo nhom va muon trao doi voi cac user khac
+             * user: muon noi la cai user hien tai muon trao doi video voi cai user o tren
+             *
+             * peerId: xac dinh thang gui la thang nao
+             * signalPayload.displayName: Xac dinh name cua thang gui
+             */
+
             const peerId = signalPayload.localUuid
-            if (peerId === this.localId
-              || (signalPayload.dest !== this.localId
-                && signalPayload.dest != this.conversationId)) {
-              console.log("break")
+            /**
+             * peerId == this.localId: chinh ban than user thi khong can phai trao doi thong tin
+             * signalPayload.dest !== this.localId && signalPayload.dest != this.conversationId -> why?
+             *  Vi du: p1 <-> p2 dang trao doi voi nhau, p3 vao phong thi luc nay
+             *  p3 send server (3, 0) -> p1, p2 nhan tin tu server roi send (1, 3), (2, 3). (**)
+             *  Ro rang tin hieu (1, 3) khong lien quan gi den p2(2) va tuong tu (2, 3) khong lien quan p1(1) -> khong can trao doi.
+             *
+             *  Va neu ban thac mac tai sao (**) p1, p2 nhan duoc p3 tai vi p3 send (3, 0). (0) la cuoc tro chuyen va
+             *  p1(1), p2(2) deu nam trong cuoc tro chuyen thi duong nhien phai nhan duoc
+             */
+
+            if (peerId === this.localId || (signalPayload.dest !== this.localId && signalPayload.dest != this.conversationId)) {
               return;
             }
-
+            /**
+             * Xac dinh diem den la gi, neu la cuoc tro chuyen(khong phai user)
+             * thi send signal toi server voi thong tin nhu sau: {
+             *   'localPeer': this.localId -> user_hien_tai
+             *   'dest': signalPayload.localUuid -> la cai thang ma muon trao doi video voi user hien tai
+             * }
+             */
             if (signalPayload.displayName && signalPayload.dest === this.conversationId) {
               //@ts-ignore
               this.setUpPeer(signalPayload.localUuid, signalPayload.displayName);
-              this.socket.emit(
-                "/signal",
-                // "/app/video-call/conversation/" + this.conversationId,
-                JSON.stringify(new SignalPayload(
-                  this.localId,
-                  this.localDisplay,
-                  signalPayload.localUuid,
-                  undefined,
-                  undefined
-                ))
-              )
-            } else if (signalPayload.displayName && signalPayload.dest === this.localId) {
+              this.socket.emit("/signal", JSON.stringify(new SignalPayload(this.localId, this.localDisplay, signalPayload.localUuid, undefined, undefined)))
+            }
+            /**
+             * signalPayload.dest == this.localId -> gui dung den user
+             */
+            else if (signalPayload.displayName && signalPayload.dest === this.localId) {
               //@ts-ignore
               this.setUpPeer(signalPayload.localUuid, signalPayload.displayName, true);
-            } else if (signalPayload.sdp) {
-              this.peerConnections[signalPayload.localUuid].pc
-                .setRemoteDescription(new RTCSessionDescription(signalPayload.sdp))
-                .then(() => {
+            }
+            /**
+             * sdp(chua media type, ip, ..vv)
+             * Co 2 loai sdp: offer, answer;
+             * offer: duoc gui tu 1 user nao do -> sau khi tao gui den user
+             * answer: chap nhan offer -> xac nhan offer va tao answer cho user do la ok, toi chap nhan
+             *
+             * Sau khi user do nhan duoc answer -> xac nhan answer -> ok
+             */
+            else if (signalPayload.sdp) {
+              this.peerConnections[signalPayload.localUuid].pc.setRemoteDescription(new RTCSessionDescription(signalPayload.sdp)).then(() => {
                   if (signalPayload.sdp.type === 'offer') {
-                    this.peerConnections[signalPayload.localUuid].pc
-                      .createAnswer()
-                      .then((description: any) => {
-                        this.createdDescription(
-                          description,
-                          signalPayload.localUuid
-                        )
-                      })
-                      .catch(errorHandler)
+                    this.peerConnections[signalPayload.localUuid].pc.createAnswer().then((description: any) => {
+                        this.createdDescription(description, signalPayload.localUuid)
+                      }).catch(errorHandler)
                   }
                 })
-            } else if (signalPayload.ice) {
-              this.peerConnections[signalPayload.localUuid].pc.addIceCandidate(
-                new RTCIceCandidate(signalPayload.ice)
-              )
-                .catch(errorHandler)
+            }
+            /**
+             * ice: Tim duong di cho viec trao doi thong tin giua cac peer(user)
+             * trong network
+             * Trong qua trinh trao doi sdp thi ice cung o trong do luon.
+             */
+            else if (signalPayload.ice) {
+              this.peerConnections[signalPayload.localUuid].pc.addIceCandidate(new RTCIceCandidate(signalPayload.ice)).catch(errorHandler)
             }
           }
         )
-        this.socket.emit(
-          "/signal",
-          JSON.stringify(new SignalPayload(
-            this.localId,
-            this.localDisplay,
-            this.conversationId,
-            undefined,
-            undefined
-          ))
-        )
+        /**
+         * Gui tin nhan bao voi server la toi muon tham gia vao cai phong nay @conversationId
+         */
+        this.socket.emit("/signal", JSON.stringify(new SignalPayload(this.localId, this.localDisplay, this.conversationId, undefined, undefined)))
       })
       .catch(errorHandler)
   }
@@ -204,7 +223,6 @@ export class FrameVideoComponent implements OnInit {
         mediaStream: event.streams[0],
         yourself: false
       })
-      console.log("-------------------stream: ", event.streams[0], "-------------------------")
     }
 
     this.mapStream.get(this.localId)?.mediaStream?.getTracks().forEach(track => {
